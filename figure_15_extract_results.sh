722a7ed9634ea78838949c36bf28bad124c64d41
Zongpu Zhang <28743569+Maphist0@users.noreply.github.com>
2023-10-30 19:42:35 -0400

    Update figure_15_extract_results.sh

--- figure_15_extract_results.sh
+++ figure_15_extract_results.sh
@@ -5,13 +5,13 @@ echo "=========================================================="
 echo "Extracting asymmetric crypto results from: $INPUT ..."
 echo
 
-echo "RSA CRT Decrypt 1K: $(pcregrep -M -A 6 'RSA.*\n.*1024$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "RSA CRT Decrypt 2K: $(pcregrep -M -A 6 'RSA.*\n.*2048$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "RSA CRT Decrypt 4K: $(pcregrep -M -A 6 'RSA.*\n.*4096$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "DH Phase-2      2K: $(pcregrep -M -A 6 'DIFFIE-HELLMAN.*\n.*2048$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "DH Phase-2      4K: $(pcregrep -M -A 6 'DIFFIE-HELLMAN.*\n.*4096$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "DSA             1K: $(pcregrep -M -A 8 'DSA.*\n.*1024$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
-echo "ECDSA          384: $(pcregrep -M -A 8 'ECDSA.*\n.*384$' $INPUT | grep Operations | awk '{ print $4 }') Mbps"
+echo "RSA CRT Decrypt 1K: $(pcregrep -M -A 6 'RSA.*\n.*1024$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "RSA CRT Decrypt 2K: $(pcregrep -M -A 6 'RSA.*\n.*2048$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "RSA CRT Decrypt 4K: $(pcregrep -M -A 6 'RSA.*\n.*4096$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "DH Phase-2      2K: $(pcregrep -M -A 6 'DIFFIE-HELLMAN.*\n.*2048$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "DH Phase-2      4K: $(pcregrep -M -A 6 'DIFFIE-HELLMAN.*\n.*4096$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "DSA             1K: $(pcregrep -M -A 8 'DSA.*\n.*1024$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
+echo "ECDSA          384: $(pcregrep -M -A 8 'ECDSA.*\n.*384$' $INPUT | grep Operations | awk '{ print $4 }') ops/s"
 
 echo
 echo "=========================================================="
